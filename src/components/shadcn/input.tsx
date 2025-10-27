8ff33c506de12c4ee1928f9e5c1c79160cb72b06
mafiefa02 <91366510+mafiefa02@users.noreply.github.com>
2025-10-27 16:49:05 +0700

    chore: fix `input` and `textarea` states design

--- src/components/shadcn/input.tsx
+++ src/components/shadcn/input.tsx
@@ -7,9 +7,9 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
 			type={type}
 			data-slot="input"
 			className={cn(
-				"h-9 w-full min-w-0 rounded-md border-2 border-input bg-transparent px-3 py-1 text-base outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-disabled",
-				"focus:border-primary focus-visible:border-primary-focused focus-visible:ring-[3px] focus-visible:ring-primary-focused/50",
-				"aria-invalid:border-destructive aria-invalid:ring-destructive/20",
+				"h-9 w-full min-w-0 rounded-md border-2 border-input bg-transparent px-3 py-1 text-base outline-none transition-[color,box-shadow] selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-disabled",
+				"focus-visible:border-primary",
+				"aria-invalid:border-destructive",
 				className,
 			)}
 			{...props}
